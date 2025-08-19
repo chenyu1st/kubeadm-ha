3fedda601d48f8c01f332d5efef299be2a241d88
TimeBye <setzero@aliyun.com>
2025-08-19 13:30:27 +0800

    [IMP]更新网络插件版本

--- offline/download-images.sh
+++ offline/download-images.sh
@@ -7,15 +7,15 @@ haproxy:2.8-alpine
 traefik:v3.0.0
 osixia/keepalived:2.0.20
 setzero/chrony:3.5
-calico/typha:v3.28.2
-calico/cni:v3.28.2
-calico/node:v3.28.2
-calico/kube-controllers:v3.28.2
-calico/ctl:v3.28.2
+calico/typha:v3.30.2
+calico/cni:v3.30.2
+calico/node:v3.30.2
+calico/kube-controllers:v3.30.2
+calico/ctl:v3.30.2
 kubernetesui/dashboard:v2.7.0
 kubernetesui/metrics-scraper:v1.0.8
-flannel/flannel:v0.25.7
-flannel/flannel-cni-plugin:v1.5.1-flannel2
+flannel/flannel:v0.27.2
+flannel/flannel-cni-plugin:v1.7.1-flannel2
 quay.io/jetstack/cert-manager-cainjector:v1.14.5
 quay.io/jetstack/cert-manager-webhook:v1.14.5
 quay.io/jetstack/cert-manager-controller:v1.14.5
@@ -29,7 +29,7 @@ registry.k8s.io/etcd:3.5.15-0
 registry.k8s.io/coredns/coredns:v1.11.3
 registry.k8s.io/ingress-nginx/controller:v1.11.3
 registry.k8s.io/ingress-nginx/kube-webhook-certgen:v1.4.4
-registry.k8s.io/metrics-server/metrics-server:v0.7.2
+registry.k8s.io/metrics-server/metrics-server:v0.8.0
 "
 
 dest_registry=${dest_registry:-'127.0.0.1:5000/kubeadm-ha'}
